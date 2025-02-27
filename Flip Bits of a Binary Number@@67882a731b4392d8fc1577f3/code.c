79021b057da3d0a744c3cab0fce138bd2fdb6c37
nidhi1447 <nidhi1447.becse24@chitkara.edu.in>
2025-02-27 14:01:48 +0530

    Solution to Flip Bits of a Binary Number

--- Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
+++ Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+unsigned int binaryToDecimal(char *binary) {
+    return strtoul(binary, NULL, 2);
+}
+
+
+void decimalToBinary(unsigned int num, char *binary, int bitSize) {
+    for (int i = bitSize - 1; i >= 0; i--) {
+        binary[i] = (num & 1) ? '1' : '0';
+        num >>= 1;
+    }
+    binary[bitSize] = '\0'; 
+}
 
 int main() {
-    unsigned int num, flipped;
+    char binary[33], flippedBinary[33]; 
+    unsigned int num, flippedNum;
+    int bitSize = 32;
+
+    
     printf("");
-    scanf("%u", &num);
-    flipped = ~num;
-    printf("%u", flipped);
+    scanf("%s", binary);
+    num = binaryToDecimal(binary);
+    flippedNum = ~num;
+    decimalToBinary(flippedNum, flippedBinary, bitSize);
+
+   
+    printf("%s", flippedBinary);
 
     return 0;
 }
+
+
